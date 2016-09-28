7ec1ff472609819a6ed92f83245efe00378251db
craft <yooobuntu@163.com>
2016-09-28 23:32:39 +0800

    +[:init] dependencies

---
+++ config/development.js
@@ -0,0 +1,5 @@
+exports.koa = require('koa');
+exports.bodyParser = require('koa-bodyparser');
+exports.logger = require('koa-logger');
+exports.router = require('koa-router');
+exports.Mongoose = require('mongoose');
