b284e77f527078e10edf5901bcd58b6ee8448130
craft <yooobuntu@163.com>
2016-10-03 10:34:06 +0800

    +[:views] add low bee pagniator

--- controllers/admin/articles.js
+++ controllers/admin/articles.js
@@ -2,9 +2,24 @@ const Article = require('models/article');
 
 class Articles {
   static *index() {
-    const articles = yield Article.find();
+    const { page = 1, per = 10} = this.query
+    const articles = yield Article.find().skip(Number((page-1)*per)).limit(Number(per));
     this.status = 200;
-    yield this.render('articles/index', { articles: articles });
+    const totalCount = yield Article.count();
+    const totalPages = Math.ceil(totalCount / per);
+    const articlePaginator = {
+      articles: articles,
+      page: page,
+      per: per,
+      totalPages: totalPages
+    };
+    if (page < totalPages) {
+      Object.assign(articlePaginator, {nextPage: Number(page)+1});
+    }
+    if (page > 1) {
+      Object.assign(articlePaginator, {prevPage: Number(page)-1});
+    }
+    yield this.render('articles/index', articlePaginator);
   }
 
   static *show() {
